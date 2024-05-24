fed742e06342a4a00fa99ce84ae1eecddeb379c8
nguyenhoangan03 <nguyenhoangan03.work@gmail.com>
2024-05-25 00:23:15 +0700

    security basic

--- src/main/java/com/adkp/fuexchange/config/WebSecurityConfig.java
+++ src/main/java/com/adkp/fuexchange/config/WebSecurityConfig.java
@@ -1,4 +1,45 @@
 package com.adkp.fuexchange.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
 public class WebSecurityConfig {
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+
+        UserDetails an = User.builder()
+                .username("a123")
+                .password("{noop}123")
+                .roles("Buyer")
+                .build();
+
+        UserDetails dung = User.builder()
+                .username("d123")
+                .password("{noop}123")
+                .roles("Seller")
+                .build();
+
+        UserDetails khanh = User.builder()
+                .username("k123")
+                .password("{noop}123")
+                .roles("Moderator")
+                .build();
+
+        UserDetails phat = User.builder()
+                .username("p123")
+                .password("{noop}123")
+                .roles("Administrator")
+                .build();
+
+        return new InMemoryUserDetailsManager(an, dung, khanh, phat);
+    }
 }
+
+
--- src/main/java/com/adkp/fuexchange/repository/RegisteredStudentRepository.java
+++ src/main/java/com/adkp/fuexchange/repository/RegisteredStudentRepository.java
@@ -3,11 +3,14 @@ package com.adkp.fuexchange.repository;
 import com.adkp.fuexchange.model.RegisteredStudent;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface RegisteredStudentRepository extends JpaRepository<RegisteredStudent, Integer> {
-    @Query("Select rs From RegisteredStudent rs")
+    @Query("Select rgtstd From RegisteredStudent rgtstd")
     List<RegisteredStudent> findAllRegisteredStudent();
 
+    @Query("Select rgtstd From RegisteredStudent rgtstd Where rgtstd.studentId.studentId = :studentId")
+    RegisteredStudent findRegisteredStudentByStudentId(@Param("studentId") String studentId);
 }
