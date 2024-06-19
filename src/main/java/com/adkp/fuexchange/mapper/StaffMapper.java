73dd6f701d7244f6b2b660b2c6de48979ff860ec
fat872003 <factnotphat0807@gmail.com>
2024-06-19 22:00:17 +0700

    crud staff

---
+++ src/main/java/com/adkp/fuexchange/mapper/StaffMapper.java
@@ -0,0 +1,13 @@
+package com.adkp.fuexchange.mapper;
+
+import com.adkp.fuexchange.dto.StaffDTO;
+import com.adkp.fuexchange.pojo.Staff;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
+
+import java.util.List;
+
+public interface StaffMapper {
+
+}
